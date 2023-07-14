39964d46889b00252e92ec6882f020e7b99b79cf
ankitasharma1999 <98891495+ankitasharma1999@users.noreply.github.com>
2023-07-14 11:51:59 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,32 @@
+
+var a=10;
+var b=20;
+//Arithmatic Operators
+console.log('Addition:',a+b)
+console.log("Suntraction:",a-b)
+console.log("Multiplication:",a*b)
+console.log("Division:",a/b)
+console.log("Reminder:",a%b)
+
+//Comparison  Operators
+console.log(a<b)
+console.log(a>b)
+console.log(a==b)
+console.log(a<=b)
+console.log(a>=b)
+
+//Logical Operators
+console.log((a<=10)&&(b>10))
+console.log((a<10)||(b<10))
+console.log(a!=1)
+
+//Bitwise Operators
+console.log(10&20)
+console.log(10==20|20==30)
+
+//String Operators
+console.log("cloud"+"eq")
+
+//Ternary operator
+var c=(a<b)?"true":"false"
+console.log(c)
